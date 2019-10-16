246991a17216965f37841d318cb955c85a7eca2d
nina <anndree.tg@gmail.com>
2019-10-16 15:08:10 +0200

    enunciado

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+/*
+Crearemos una aplicación web que nos va a permitir jugar a Piedra, Papel o Tijeras contra la máquina!! La web constará de:
+
+3 botones con las opciones "Piedra", "Papel" y "Tijeras". 
+
+Cuando el usuario haga click en alguno de estos botones, se guardará la jugada del jugador en una variable, y se generará aleatoriamente la jugada de la máquina, que se guardará en otra variable. 
+
+Con la función de árbitro que hicisteis ayer, compararéis la jugada del jugador con la jugada de la máquina. El resultado de la partida se imprimirá en la pantalla. 
+
+También haremos un contador de victorias. En las cookies se le pondrá al usuario un contador de victorias, que comenzará en cero. Cada vez que el usuario gane una partida, se incrementará en 1 el valor de esta cookie. También tendrá un contador de derrotas guardado en las cookies. Cada vez que el usuario pierda, se incrementará en uno el valor de esta cookie.
+
+Encima de los 3 botones pondremos los contadores de victorias y de derrotas. 
+
+*IMPORTANTE* Crear un repo de git solamente para este proyecto, a parte de los otros repos de git que tenéis.
+
+
+
+
+
+Enviar mensaje a bcn-full-stack-boot
+
+
+
+*/
